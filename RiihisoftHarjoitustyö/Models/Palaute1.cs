de3e256e51fa0fbf072440ca64a41ef36707c8f9
Jeremi Andersin <61796625+ju1pp1@users.noreply.github.com>
2022-08-23 15:36:42 +0300

    Commit

---
+++ RiihisoftHarjoitustyö/Models/Palaute1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RiihisoftHarjoitustyö.Models
+{
+    public partial class Palaute1
+    {
+        public int Id { get; set; }
+        public string Etunimi { get; set; } = null!;
+        public string Sukunimi { get; set; } = null!;
+        public string Sähköposti { get; set; } = null!;
+        public string AvoinPalaute { get; set; } = null!;
+    }
+}
---
+++ RiihisoftHarjoitustyö/Models/Tiedot.cs
@@ -0,0 +1,11 @@
+﻿namespace RiihisoftHarjoitustyö.Models
+{
+    public class Tiedot
+    {
+        public int Id { get; set; }
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Sähköposti { get; set; }
+        public string AvoinPalaute { get; set; }
+    }
+}
